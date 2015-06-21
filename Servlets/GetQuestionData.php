8c6068bf93a3e7b8ca478bd634080b5743a8ec7e
taniags93 <tanita93@gmail.com>
2015-06-21 23:23:19 +0200

    error

--- Servlets/GetQuestionData.php
+++ Servlets/GetQuestionData.php
@@ -13,7 +13,7 @@
 	if($Database->checkIfNotEmpty($query)){
 		$result = mysqli_fetch_assoc($Database->select($what,$from,$where));
 		$options = $QuizDao->ReadAllOption($QuestionID);
-		$final_result = array('success' => true, 'Title' => $result['Title'], 'Options'=>$options));
+		$final_result = array('success' => true, 'Title' => $result['Title'], 'Options'=>$options);
 		echo json_encode($final_result);
 	}else{
 		echo json_encode(array('success' => false));
